afe71e39c6981061173f6984eba08a845f5a4c07
Srinithin.R.M <srinithinrms@gmail.com>
2026-02-27 08:36:08 +0530

    Set base path for deployment in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+        
+      base: '/agri-aura/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
